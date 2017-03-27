9e1c7c65f735b77ef71db5090bece6d049891fad
RoyerUnivalle <royer.estrada@correounivalle.edu.co>
2017-03-27 09:54:32 -0500

    servicio

--- app/src/main/java/com/example/userasus/asynctask/MainActivity.java
+++ app/src/main/java/com/example/userasus/asynctask/MainActivity.java
@@ -1,17 +1,22 @@
 package com.example.userasus.asynctask;
 
+import android.app.Service;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.AsyncTask;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.example.userasus.asynctask.Services.Horario;
 
 import java.util.Random;
 
@@ -20,14 +25,19 @@ public class MainActivity extends AppCompatActivity {
 
     LinearLayout linear;
     TextView valor;
-    Pintar obj = new Pintar();
+    EditText campo;
+    Pintar obj = null;
     Boolean on=false;
+    Boolean flagService=false;
+    Button btn;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         linear = (LinearLayout) findViewById(R.id.cuadro);
         valor = (TextView) findViewById(R.id.tvEscribe);
+        btn = (Button) findViewById(R.id.btn_service);
+        campo = (EditText) findViewById(R.id.editText);
     }
     public void colorear(View v) throws InterruptedException {
         for(int h=0;h<=199;){
@@ -37,13 +47,15 @@ public class MainActivity extends AppCompatActivity {
         }
     }
     public void colorear2(View v){
-        if(on){
-            obj.cancel(true);
-            obj.execute();
-        }else{
-            obj.execute();
-            on= true;
+
+        if(flagService){
+            stopService(new Intent(MainActivity.this,Horario.class));
+            flagService=false;
+            btn.setBackgroundColor(Color.rgb(0,255,0));
         }
+            obj = new Pintar();//creando una nuvea instancia se soluciona el problema  Cannot execute task: the task is already running.
+            obj.execute();
+            on=true;
     }
 
     @Override
@@ -51,6 +63,25 @@ public class MainActivity extends AppCompatActivity {
         super.onDestroy();
         if(on){
             obj.cancel(true);
+            on=false;
+        }
+    }
+
+    public void Limpiar(View v){
+        valor.setText(""+" ");
+        campo.setText(""+" ");
+    }
+
+    public void LlamarServicio(View v){
+        if(flagService){
+            stopService(new Intent(MainActivity.this,Horario.class));
+            flagService=false;
+            btn.setBackgroundColor(Color.rgb(0,255,0));
+        }else{
+            startService(new Intent(MainActivity.this,Horario.class));
+            Toast.makeText(this,"ON",Toast.LENGTH_LONG).show();
+            flagService=true;
+            btn.setBackgroundColor(Color.rgb(255,0,0));
         }
     }
 
@@ -81,12 +112,16 @@ public class MainActivity extends AppCompatActivity {
         protected Void doInBackground(Void... voids) {
             int x=0;
             while (x<=199){
-                publishProgress(x);//Llama a >>>>> onProgressUpdate
-                try {
-                    Thread.sleep(50);
-                    x++;
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+                if(obj.isCancelled()){
+                    break;
+                }else{
+                    publishProgress(x);//Llama a >>>>> onProgressUpdate
+                    try {
+                        Thread.sleep(50);
+                        x++;
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
                 }
             }
             return null;
@@ -100,7 +135,6 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onCancelled() {
             super.onCancelled();
-                on=false;
                 obj=null;
         }
         public int generarAleatorio(){
--- app/src/main/java/com/example/userasus/asynctask/Services/Horario.java
+++ app/src/main/java/com/example/userasus/asynctask/Services/Horario.java
@@ -2,9 +2,17 @@ package com.example.userasus.asynctask.Services;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.IBinder;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Horario extends Service {
+
+    MostrarHora myTask=null;
+
     public Horario() {
     }
 
@@ -12,5 +20,74 @@ public class Horario extends Service {
     public IBinder onBind(Intent intent) {
         // TODO: Return the communication channel to the service.
         throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Toast.makeText(this,"Servicio creado",Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        MostrarHora hora = new MostrarHora();
+        myTask=hora;
+        myTask.execute();
+        String estado= myTask.getStatus().toString();
+        Toast.makeText(this, "onStartCommand! : "+estado, Toast.LENGTH_SHORT).show();
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        myTask.cancel(true);
+        Toast.makeText(this,"Servicio destruido",Toast.LENGTH_LONG).show();
+    }
+
+    public  class MostrarHora extends AsyncTask<Void,String,Void>{
+
+        private SimpleDateFormat dateFormat;
+        private String date;
+        private boolean cent;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            dateFormat = new SimpleDateFormat("HH:mm:ss");
+            cent = true;
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            int i=0;
+            while (cent){
+                if(myTask.isCancelled()){
+                    break;
+                }
+                date = dateFormat.format(new Date());
+                try {
+                    publishProgress(date);
+                    // Stop 5s
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                i++;
+            }
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(String... values) {
+            super.onProgressUpdate(values);
+            Toast.makeText(getApplicationContext(), "Hora actual: " + values[0], Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        protected void onCancelled() {
+            super.onCancelled();
+            cent=false;
+        }
     }
 }
