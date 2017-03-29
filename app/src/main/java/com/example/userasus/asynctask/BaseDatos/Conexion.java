c19f0344a623244f2d2ac7806380156b67c46d1e
RoyerUnivalle <royer.estrada@correounivalle.edu.co>
2017-03-29 14:46:08 -0500

    base de datos

---
+++ app/src/main/java/com/example/userasus/asynctask/BaseDatos/Conexion.java
@@ -0,0 +1,7 @@
+package com.example.userasus.asynctask.BaseDatos;
+
+/**
+ * Created by UserAsus on 28/03/2017.
+ */
+public class Conexion {
+}
---
+++ app/src/main/java/com/example/userasus/asynctask/Data/Conection.java
@@ -0,0 +1,7 @@
+package com.example.userasus.asynctask.Data;
+
+/**
+ * Created by UserAsus on 29/03/2017.
+ */
+public class Conection {
+}
